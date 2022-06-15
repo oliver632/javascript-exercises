e0747d527d0efa8dc17383157a487ccb2a877264
Oliver Frost <oliverfrost11@gmail.com>
2022-06-15 23:33:51 +0100

    Finished 12. exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    arrayOfAges = [];
+    let oldAge = 0;
+    let oldest;
+    for (x in people) {
+        let age = 0;
+        if (isNaN(people[x].yearOfDeath)) {
+            age = (new Date().getFullYear())-people[x].yearOfBirth;
+        } else {
+            age = people[x].yearOfDeath-people[x].yearOfBirth;
+        }
+        if (age>oldAge) {
+            oldest = x;
+            oldAge = age;
+        } else {
+            continue;
+        }
+        
+    }
+    return people[oldest];
 };
+//console.log(new Date().getFullYear());
+const peeps = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1950,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+  console.log(findTheOldest(peeps));
 // Do not edit below this line
 module.exports = findTheOldest;

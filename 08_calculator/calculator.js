f11ca1b657ab8774608b0583662d81f87c3dd377
Oliver Frost <oliverfrost11@gmail.com>
2022-06-15 22:25:27 +0100

    Finished 8. exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+    let som = 0;
+    for(let i = numbers[0].length-1;i>-1;i--){
+        som += numbers[0][i];
+    };
+    return parseInt(som);
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+    numbers = numbers[0];
+    let sum = numbers[numbers.length-1];
+    for(let i = numbers.length-2;i>=0;i--){
+        sum *= numbers[i];
+    };
+    return parseInt(sum);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+    return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = num;
+    for(let i = num-1;i > 0; i--){
+        sum *= i;
+    };
+    return ((num === 0) ? 1 : sum);
 };
 
 // Do not edit below this line

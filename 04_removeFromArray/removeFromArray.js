f3f1564fc48c2aa3d5cdcb290c5913220ce3c2cc
Oliver Frost <oliverfrost11@gmail.com>
2022-06-15 19:10:34 +0100

    Finished 4. exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, remove, ...removeToo) {
+    let originalArray = theArray;
+    let toBeRemoved = new Array().concat(remove);
+    toBeRemoved = toBeRemoved.concat(removeToo);
+    for(let i = toBeRemoved.length-1;i>=0; i--){
+        while(originalArray.indexOf(toBeRemoved[i])>-1){
+            originalArray.splice(originalArray.indexOf(toBeRemoved[i]),1);
+        };
 
+    };
+    return originalArray;
 };
+removeFromArray([1,2,3,4],3);
 
 // Do not edit below this line
 module.exports = removeFromArray;

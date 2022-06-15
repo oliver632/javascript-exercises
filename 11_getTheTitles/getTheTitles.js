90d58432342839b50796af9fc73e203e49fc0f98
Oliver Frost <oliverfrost11@gmail.com>
2022-06-15 23:15:24 +0100

    Finished 11. exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -5,16 +5,6 @@ const getTheTitles = function(books) {
       }
       return bookArray;
 };
-const books = [
-    {
-      title: 'Book',
-      author: 'Name'
-    },
-    {
-      title: 'Book2',
-      author: 'Name2'
-    }
-  ]
-console.log(getTheTitles(books));
+
 // Do not edit below this line
 module.exports = getTheTitles;

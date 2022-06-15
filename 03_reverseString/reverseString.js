5f260d19ef2e4f48c7f7bd8e5fe91d1a1bb89eb9
Oliver Frost <oliverfrost11@gmail.com>
2022-06-15 18:23:33 +0100

    Finished 3. Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = [];
+    let stringLength = string.length;
+    for(let i = 0;i<stringLength;i++){
+        stringArray[i] = string[i];
+    }
+    let newString = "";
+    for(let i = stringLength-1;i>=0;i--){
+        newString = newString.concat(stringArray[i]);
+    }
+    return newString;
 };
 
+reverseString("test");
 // Do not edit below this line
 module.exports = reverseString;

258de1af7289a94faec67c5ded213fce45873ba8
Oliver Frost <oliverfrost11@gmail.com>
2022-06-15 22:36:38 +0100

    Finished 9. exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replaceAll(".", "").toLowerCase();
+    word = word.replaceAll(" ", "");
+    word = word.replaceAll("!", "");
+    word = word.replaceAll(",", "");
+    let reversedWord = word.split("").reverse().join("");
+    if(word === reversedWord){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

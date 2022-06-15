32314a273b10b33219caffaa908757a952c73523
Oliver Frost <oliverfrost11@gmail.com>
2022-06-15 22:50:33 +0100

    Finished 10. exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    fiboArray = [1,1];
+    console.log(fiboArray);
+    for (let i = num;i>0;i--) {
+        console.log(fiboArray);
+        numForFibo = parseInt(fiboArray[fiboArray.length-1])+parseInt(fiboArray[fiboArray.length-2]);
+        fiboArray = fiboArray.concat(numForFibo);
+    }
+    return fiboArray[num-1];
 };
-
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;

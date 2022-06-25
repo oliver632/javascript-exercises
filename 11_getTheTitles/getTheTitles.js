60faaee33ff48f9bfb6b571f36b76e6f6d9cd474
Oliver Frost <oliverfrost11@gmail.com>
2022-06-25 20:49:49 +0200

    Changed to .map usage to incorporate learning

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,12 +1,9 @@
 const getTheTitles = function(books) {
   //making array for for...in loop
-    let bookArray = new Array();
+    let bookArray = books.map(book => book.title);
     //for each book in books, add it to the above array. 
     //an alternative could be using the .map() method.
-    for(book in books){
-        bookArray = bookArray.concat((books[book].title));
-      }
-      return bookArray;
+    return bookArray;
 };
 
 // Do not edit below this line

f2bc287275579039a2c5febab77db2191a1e59df
Oliver Frost <oliverfrost11@gmail.com>
2022-06-25 21:12:13 +0200

    Added .reduce method to make simpler and incorporate new learning

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,29 +1,19 @@
 const findTheOldest = function(people) {
-    arrayOfAges = [];
-    //make variables for for loop.
-    let oldAge = 0;
-    let oldest;
-    for (x in people) {
-        let age = 0;
-        //if they are not dead, get age from current date.
-        if (isNaN(people[x].yearOfDeath)) {
-            age = (new Date().getFullYear())-people[x].yearOfBirth;
-        } else {
-        //if  dead, get their age from birth and death year.
-            age = people[x].yearOfDeath-people[x].yearOfBirth;
-        }
-        //if current older than previous, replace, otherwise keep. 
-        //This makes sure to get the oldest after all are checked.
-        if (age>oldAge) {
-            oldest = x;
-            oldAge = age;
-        } else {
-            continue;
-        }
-        
-    }
-    //return the oldest person found.
-    return people[oldest];
+
+    return people.reduce((oldest, current) => {
+        //get age of current person
+        let currentAge = (!(current.yearOfDeath) 
+        ? (new Date().getFullYear())
+        : current.yearOfDeath)-current.yearOfBirth;
+
+        //get age of oldest person
+        let oldestAge = (!(oldest.yearOfDeath) 
+        ? (new Date().getFullYear())
+        : oldest.yearOfDeath)-oldest.yearOfBirth;
+
+        //return new oldest
+        return oldestAge < currentAge ? current : oldest;
+    })
 };
 
 // Do not edit below this line
